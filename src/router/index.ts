1e453906d349ab7c649d4e5c76849b178429edcd
nsefelibata <937742413@qq.com>
2022-06-01 08:39:48 +0000

    Deploying to dev from @ nsefelibata/cdn-test@07510ed4964767ed02a0812cd587b890cefd3d04 🚀

--- src/router/index.ts
+++
@@ -1,8 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import constantRoutes from './constant'
-const router = createRouter({
-    routes: constantRoutes,
-    history: createWebHistory()
-})
-
-export default router
